bec18b596b81683f8b6ba8eb4ad9c959ba37ac3a
userquin <userquin@gmail.com>
2026-05-01 18:36:58 +0200

    chore: add initial generate manifest entries tests

--- packages/workbox/build/test/generate-manifest-entries.spec.ts
+++ packages/workbox/build/test/generate-manifest-entries.spec.ts
@@ -1,42 +1,70 @@
-import { deepMergeObject } from 'magicast/helpers'
-import { describe, expect, it } from 'vitest'
-import { generateManifestEntries } from '../src/utils/generate-manifest-entries'
-import { validateGetManifest } from '../src/validation/validation-helper'
-import { createGetManifestOptions, generateSWFixture, injectManifestFixture } from './test-helper'
-/*
 import type {
   GenerateSWOptions,
   GetManifestOptions,
   InjectManifestOptions,
-} from '../src/types'
-import { validateGenerateSW, validateGetManifest } from '../src/validation/validation-helper'
-import { createGenerateSWOptions, createGetManifestOptions } from './test-helper'
-*/
+} from '@composable-vite-pwa/workbox-build/types'
+import { deepMergeObject } from 'magicast/helpers'
+import { describe, expect, it } from 'vitest'
+import { generateManifestEntries } from '../src/utils/generate-manifest-entries'
+import { validateGenerateSW, validateGetManifest, validateInjectManifest } from '../src/validation/validation-helper'
+import {
+  createGenerateSWOptions,
+  createGetManifestOptions,
+  createInjectManifestOptions,
+  generateSWFixture,
+  injectManifestFixture,
+} from './test-helper'
 
 describe('generate-manifest-entries results', () => {
-  describe('get-manifest', () => {
-    it ('generates valid manifest entries', async () => {
-      const options = createGetManifestOptions({
-        globDirectory: generateSWFixture,
-        globPatterns: ['**/*.{js,html}'],
-      })
-      const validatedOptions = await validateGetManifest(options.options)
-      deepMergeObject(options.options, validatedOptions)
-      let promise = generateManifestEntries(
-        options.options,
-        options.globDirectory,
-      )
-      await expect(promise).resolves.not.toThrow()
-      let entries = await promise
-      expect(entries.manifestEntries.length).toBeGreaterThan(0)
-      options.globDirectory = injectManifestFixture
-      promise = generateManifestEntries(
-        options.options,
-        options.globDirectory,
-      )
-      await expect(promise).resolves.not.toThrow()
-      entries = await promise
-      expect(entries.manifestEntries.length).toBeGreaterThan(0)
+  async function runWith<T extends GetManifestOptions | InjectManifestOptions | GenerateSWOptions<'classic'>>(
+    createFn: (opts?: T) => { options: T },
+    validateFn: (opts: T) => Promise<any>,
+    customOptions: Partial<T> = {},
+  ) {
+    const { options } = createFn({
+      globPatterns: ['**/*.{js,html}'],
+      ...customOptions,
+    } as T)
+    const validated = await validateFn(options)
+    deepMergeObject(options, validated)
+
+    const promise = generateManifestEntries(options, options.globDirectory)
+    await expect(promise).resolves.not.toThrow()
+    const result = await promise
+
+    return { options, result }
+  }
+  it ('get-manifest: handles generate-sw and inject-manifest fixtures correctly', async () => {
+    const { result: r1 } = await runWith(createGetManifestOptions, validateGetManifest, {
+      globDirectory: generateSWFixture,
     })
+    expect(r1.manifestEntries.map(n => n.url)).toEqual(['index.html', 'index.js'])
+
+    const { result: r2 } = await runWith(createGetManifestOptions, validateGetManifest, {
+      globDirectory: injectManifestFixture,
+    })
+    expect(r2.manifestEntries.map(n => n.url)).toEqual(['index.html', 'index.js', 'sw.js'])
+
+    const { result: r3 } = await runWith(createGetManifestOptions, validateGetManifest, {
+      globDirectory: injectManifestFixture,
+      globIgnores: ['sw.js'],
+    })
+    expect(r3.manifestEntries.map(n => n.url)).toEqual(['index.html', 'index.js'])
+  })
+  it ('generate-sw: handles generate-sw fixture correctly', async () => {
+    const { result } = await runWith(
+      () => createGenerateSWOptions('classic'),
+      validateGenerateSW,
+    )
+    expect(result.manifestEntries.map(n => n.url)).toEqual(['index.html', 'index.js'])
+  })
+  it ('inject-manifest: handles inject-manifest fixture correctly', async () => {
+    const { result: r1 } = await runWith(createInjectManifestOptions, validateInjectManifest)
+    expect(r1.manifestEntries.map(n => n.url)).toEqual(['index.html', 'index.js', 'sw.js'])
+
+    const { result: r2 } = await runWith(createInjectManifestOptions, validateInjectManifest, {
+      globIgnores: ['sw.js'],
+    })
+    expect(r2.manifestEntries.map(n => n.url)).toEqual(['index.html', 'index.js'])
   })
 })
