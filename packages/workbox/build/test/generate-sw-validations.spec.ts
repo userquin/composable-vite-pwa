8c2b273861a204465ac563d3104f53742725e3de
userquin <userquin@gmail.com>
2026-05-01 12:19:03 +0200

    chore: add initial generateSW validations

--- packages/workbox/build/test/generate-sw-validations.spec.ts
+++ packages/workbox/build/test/generate-sw-validations.spec.ts
@@ -5,6 +5,45 @@ import { DEFAULT_MAXIMUM_FILE_SIZE_TO_CACHE_IN_BYTES } from '../src/utils/consta
 import { AsyncGenerateSWOptionsSchema } from '../src/validation/async-generate-sw'
 import { validateGenerateSW } from '../src/validation/validation-helper'
 
+function generateSWOptions<T extends SWType>(
+  swType: T,
+  withGlobDirectory = true,
+  options: Partial<GenerateSWOptions<T>> = {},
+) {
+  const swDest = path.relative(
+    process.cwd(),
+    path.resolve(import.meta.dirname, 'fixtures/fixture-generate-sw', 'sw.js'),
+  ).replace(/\\/g, '/')
+  return {
+    swDest,
+    options: Object.assign(
+      {},
+      {
+        swType,
+        globDirectory: withGlobDirectory
+          ? path.resolve(import.meta.dirname, 'fixtures/fixture-generate-sw').replace(/\\/g, '/')
+          : undefined,
+        swDest,
+      },
+      options,
+    ) satisfies GenerateSWOptions<T>,
+  }
+}
+
+function withDummyRuntimeCaching<T extends SWType>(
+  swType: T,
+  withGlobDirectory = false,
+  options: Partial<GenerateSWOptions<T>> = {},
+) {
+  const data = generateSWOptions(swType, withGlobDirectory, options)
+  data.options.runtimeCaching = [{
+    handler: 'NetworkOnly',
+    method: 'GET',
+    urlPattern: /.*/,
+  }]
+  return data
+}
+
 describe('generate-sw validations', () => {
   const objectSchema = AsyncGenerateSWOptionsSchema.pipe[0]
   const entries = objectSchema.entries
@@ -15,22 +54,39 @@ describe('generate-sw validations', () => {
   it('required fields', () => {
     expect(requiredFields).toEqual(['swDest'])
   })
-  it('default values', async () => {
-    const swDest = path.relative(
-      process.cwd(),
-      path.resolve(import.meta.dirname, 'fixtures/fixture-generate-sw', 'sw.js'),
-    ).replace(/\\/g, '/')
-    const options = {
-      globDirectory: path.resolve(import.meta.dirname, 'fixtures/fixture-generate-sw').replace(/\\/g, '/'),
-      swDest,
-    } satisfies GenerateSWOptions<SWType>
+
+  const swTypes: SWType[] = ['classic', 'module', 'classic-and-module']
+
+  it.each(swTypes)('default values are populated for %s', async (swType) => {
+    const { swDest, options } = generateSWOptions(swType)
     await expect(validateGenerateSW(options)).resolves.toMatchObject({
       swDest,
-      swType: 'classic',
+      swType,
       maximumFileSizeToCacheInBytes: DEFAULT_MAXIMUM_FILE_SIZE_TO_CACHE_IN_BYTES,
       throwMaximumFileSizeToCacheInBytes: true,
       globPatterns: ['**/*.{js,css,html}'],
       globIgnores: ['**/node_modules/**/*'],
-    } satisfies GenerateSWOptions<SWType>)
+    })
+  })
+
+  it.each(swTypes)('missing swDest folder fails for %s', async (swType) => {
+    const { options } = generateSWOptions(swType, true, {
+      swDest: '__missing__/sw.js',
+    })
+    await expect(validateGenerateSW(options)).rejects.toThrow(
+      /The 'swDest' value must be a valid path/,
+    )
+  })
+
+  it.each(swTypes)('missing globDirectory and runtimeCaching fails for %s', async (swType) => {
+    const { options } = generateSWOptions(swType, false)
+    await expect(validateGenerateSW(options)).rejects.toThrow(
+      /Couldn't find configuration for either precaching or runtime caching/,
+    )
+  })
+
+  it.each(swTypes)('missing globDirectory with a runtimeCaching does NOT fail for %s', async (swType) => {
+    const { options: classicOptions } = withDummyRuntimeCaching(swType)
+    await expect(validateGenerateSW(classicOptions)).resolves.not.toBeUndefined()
   })
 })
