6b717f9f2ff935d31d98b05ff543e8d92d5aa65c
userquin <userquin@gmail.com>
2026-05-13 01:06:19 +0200

    chore(build): add envDir and envPrefix to rolldown build types

--- packages/workbox/build/src/build/rolldown/types.ts
+++ packages/workbox/build/src/build/rolldown/types.ts
@@ -3,6 +3,8 @@ import type { BuildSWOptions } from '../types'
 
 export interface ServiceWorkerOptions {
   define?: import('rolldown').TransformOptions['define']
+  envDir?: string | false
+  envPrefix?: string | string[]
   plugins?: () => import('rolldown').Plugin[]
   sourcemap?: import('rolldown').OutputOptions['sourcemap']
 }
