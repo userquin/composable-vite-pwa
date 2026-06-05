5a7e9d6a4b162a38a274ec79421f02bf836afbea
userquin <userquin@gmail.com>
2026-06-05 10:22:58 +0200

    chore: change valibot validation to use function

--- packages/workbox/build/src/validation/utils.ts
+++ packages/workbox/build/src/validation/utils.ts
@@ -281,7 +281,7 @@ export const ManifestOptionsSchema = v.strictObject({
   /**
    * Async generator that yields additional entries to be preached
    */
-  additionalManifestEntriesGenerator: v.optional(v.any()),
+  additionalManifestEntriesGenerator: v.optional(v.function()),
   /**
    * Assets that match this will be assumed to be uniquely versioned via their
    * URL, and exempted from the normal HTTP cache-busting that's done when
