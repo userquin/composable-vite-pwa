2290666a941d7e9fa07845af7f8cd8775e8a3f12
userquin <userquin@gmail.com>
2026-06-10 20:53:35 +0200

    chore: cleanup vite legacy ts sw

--- examples/vite-ts-legacy/src/sw.ts
+++ examples/vite-ts-legacy/src/sw.ts
@@ -1,21 +1,14 @@
-/// <reference lib="webworker" />
-import type { PrecacheEntry } from '@composable-vite-pwa/workbox-swkit/precaching'
 import { clientsClaim } from '@composable-vite-pwa/workbox-swkit/core'
 import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from '@composable-vite-pwa/workbox-swkit/precaching'
 import { NavigationRoute, registerRoute } from '@composable-vite-pwa/workbox-swkit/routing'
 import { message } from 'virtual:message'
 import { hello, sayHello } from './sw-helper.ts'
 
-declare global {
-  interface Window {
-    __WB_MANIFEST: Array<PrecacheEntry | string>
-    skipWaiting: () => Promise<void>
-  }
-}
-
 console.log(message)
 console.log(hello)
 console.log(sayHello('from SW'))
+
+declare let self: ServiceWorkerGlobalScope
 
 // self.__WB_MANIFEST is default injection point
 precacheAndRoute(self.__WB_MANIFEST)
