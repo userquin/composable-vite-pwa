fe4b7439f7c78d8eeedf7595e3d08ab98ebd0592
userquin <userquin@gmail.com>
2026-06-14 12:43:07 +0200

    chore: use writeBundle hook when using rolldown bundler with sourcemap enabled and classic sw

--- packages/workbox/build/src/build/builder/prepare-rolldown-output-options.ts
+++ packages/workbox/build/src/build/builder/prepare-rolldown-output-options.ts
@@ -99,9 +99,12 @@ export async function prepareRolldownOutputOptions<B extends Bundler>(
     destFolder,
     customChunksInfo,
     classicBuild,
+    sourcemap,
   }))
 
   if (bundler === 'rolldown') {
+    // TODO: check rolldown version to include this plugin once
+    //       https://github.com/oxc-project/oxc/issues/23224 at rolldown
     plugins.unshift(await import('./rolldown-render-chunk-plugin').then(({
       RolldownRenderChunkPlugin,
     }) => RolldownRenderChunkPlugin(
