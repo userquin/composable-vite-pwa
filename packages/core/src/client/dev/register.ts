e754876edccd09c221b70ad89b6950737e347cf3
userquin <userquin@gmail.com>
2026-06-20 21:47:38 +0200

    chore: remove core package

--- packages/core/src/client/dev/register.ts
+++
@@ -1,6 +0,0 @@
-import type { RegisterSWOptions } from '../types'
-
-export type { RegisterSWOptions }
-
-export function registerSW(_options: RegisterSWOptions = {}) {
-}
--- packages/core/types/core-client.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference path="./importMeta.d.ts" />
