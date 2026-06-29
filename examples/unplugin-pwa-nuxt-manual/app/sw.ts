74dafb0d3db84f6a3dd793862b6d016c0a8aad2c
Houtan Rocky <houtan.rocky@gmail.com>
2026-06-29 08:14:27 +0330

    chore(example): add chunked entry processing arguments to example

--- examples/unplugin-pwa-nuxt-manual/app/sw.ts
+++ examples/unplugin-pwa-nuxt-manual/app/sw.ts
@@ -25,6 +25,8 @@ if (import.meta.env.DEV) {
 precacheAndRoute(
   self.__WB_MANIFEST,
   {
+    chunked: true,
+    chunkSize: 5,
     urlManipulation: ({ url }) => {
       const urls: URL[] = []
       if (url.pathname.endsWith('_payload.json')) {
