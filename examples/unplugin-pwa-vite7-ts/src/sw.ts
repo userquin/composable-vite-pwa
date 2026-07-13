1fcfc2746fea086af7e83eb8147bf1bdb658bfc0
userquin <userquin@gmail.com>
2026-07-13 12:19:29 +0200

    chore: add vite 6 and 7 examples

---
+++ examples/unplugin-pwa-vite7-ts/src/sw.ts
@@ -0,0 +1,30 @@
+import { clientsClaim } from '@composable-vite-pwa/workbox-swkit/core'
+import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from '@composable-vite-pwa/workbox-swkit/precaching'
+import { NavigationRoute, registerRoute } from '@composable-vite-pwa/workbox-swkit/routing'
+import { message } from 'virtual:message'
+import { hello, sayHello } from './sw-helper.ts'
+
+console.log(message)
+console.log(hello)
+console.log(sayHello('from SWx'))
+
+declare let self: ServiceWorkerGlobalScope
+
+// self.__WB_MANIFEST is default injection point
+precacheAndRoute(self.__WB_MANIFEST)
+
+// clean old assets
+cleanupOutdatedCaches()
+
+let allowlist: undefined | RegExp[]
+if (import.meta.env.DEV)
+  allowlist = [/^\/$/]
+
+// to allow work offline
+registerRoute(new NavigationRoute(
+  createHandlerBoundToURL('index.html'),
+  { allowlist },
+))
+
+self.skipWaiting()
+clientsClaim()
